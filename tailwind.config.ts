378ba990eae80583c226c71da00c35332c5883da
kimsunin <suninkim10@gmail.com>
2024-10-23 16:32:39 +0900

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      screens: {
+        mobile: { max: "767px" },
+        tablet: { min: "768px", max: "1023px" },
+        pc: { min: "1024px" },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
